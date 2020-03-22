9b4878232622223190a65c399c80eae01b54e6c3
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-22 17:39:32 +0300

    init:"start brackets task"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = []
+  for(var i = 0 ; i < bracketsConfig.length ; i++){
+    brackets.push(bracketsConfig[i][0]+bracketsConfig[i][1]);
+  }
+  for (var i = 0; i < str.length; i++) {
+    if( str.includes(brackets[i]) ) {
+      str = str.replace(brackets[i], "");
+      i = -1;
+    }
+  }
+  if(str.length === 0){return true}
+  else{return false}
 }
